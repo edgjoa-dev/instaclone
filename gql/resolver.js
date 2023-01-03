98a91801d3bef0f7fa8f3829594a3c12fa195401
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2023-01-03 14:46:20 -0600

    Dev: Creacion de mutation para registro de usuario en graphql

--- gql/resolver.js
+++ gql/resolver.js
@@ -5,8 +5,16 @@ const resolvers = {
         getUser: ()=> {
             console.log('Obteniendo usuario');
             return null;
-        }
-    }
+        },
+    },
 
+    Mutation: {
+        register: ( _, {input} )=>{
+            //User registered
+            console.log('Registro de usuario ok');
+            console.log(input);
+            return input;
+        },
+    },
 }
 module.exports = resolvers;
--- gql/schema.js
+++ gql/schema.js
@@ -6,10 +6,28 @@ const typeDefs = gql`
         id: ID
         name: String
         userName: String
+        email: String
+        avatar: String
+        siteWeb: String
+        description: String
+        password: String
+        createAt: String
     }
-    type Query {
 
+    input UserInput {
+        name: String!
+        userName: String!
+        email: String!
+        password: String!
+    }
+
+
+    type Query {
         getUser: User
+    }
+
+    type Mutation {
+        register(input: UserInput): User
     }
 
 `;
