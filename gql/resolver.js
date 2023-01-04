6bd3e797d868c81f2ed785391fd9e3c12f747df4
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2023-01-03 18:51:19 -0600

    Dev: Creando controllers para usuario

--- gql/resolver.js
+++ gql/resolver.js
@@ -1,5 +1,4 @@
-const User = require('../models/user')
-const bcryptjs = require('bcryptjs')
+const userController = require('../controllers/user')
 
 const resolvers = {
 
@@ -12,36 +11,8 @@ const resolvers = {
     },
 
     Mutation: {
-        register: async( _, {input} )=>{
-        //Registro de uauario
-        const newUser = input
-
-        newUser.email = newUser.email.toLowerCase();
-        newUser.userName = newUser.userName.toLowerCase();
-
-        const { email, userName, password } = newUser
-
-        //validar si el email ya esta en uso
-        const emailValid = await User.findOne({email})
-        if(emailValid) throw new Error('email ya se encuentra registrado')
-        //validar si el userNasme ya esta en uso
-        const userNameValid = await User.findOne({email})
-        if(userNameValid) throw new Error('nombre de usuario ya se encuentra registrado')
-
-        //Encriptar el password
-        const salt = await bcryptjs.genSaltSync(10);
-        newUser.password = await bcryptjs.hash(password, salt);
-
-        //Guardar newUser en DB
-        try {
-            const user = new User(newUser)
-            user.save();
-            return user;
-        } catch (error) {
-            console.log(error);
-            }
+        register: async( _, {input}) => userController.register(input)
     },
-},
 
 }
 module.exports = resolvers;
